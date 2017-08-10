c5c83d71ef8920e4b577e8985f2ec6bd1325d23e
Kevin Zhou <kevin.zhou.888@gmail.com>
2017-08-10 04:32:19 +0000

    Remove unrelated code

--- mymobxapp/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ToDoApp from './ToDoApp';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<ToDoApp />, document.getElementById('root'));
-registerServiceWorker();
